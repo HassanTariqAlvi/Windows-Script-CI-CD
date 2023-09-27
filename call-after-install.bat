dcdf1ca99e5fed757ffa4bd6fdc88f7a33e9f193
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 14:42:01 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -3,9 +3,6 @@
 REM Write to C:\xyz.txt
 echo Working > C:\xyz.txt
 
-REM Add Node.js and Git to the PATH
-set PATH=%PATH%;C:\Program Files\nodejs;C:\Program Files\Git\cmd
-
 REM Navigate to the project directory and execute git and npm commands
 cd C:\Project\Project-1
 call deploy.bat
