8353e47ff2556cc2bb98c21a57502ca655c28260
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 15:37:40 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -3,10 +3,10 @@
 REM Write to C:\xyz.txt
 echo Working > C:\xyz.txt
 
-set PATH=%PATH%;C:\Program Files\Git;
+
 
 REM Navigate to the project directory and execute git and npm commands
 cd C:\Project\Project-1
-REM git config --global --add safe.directory C:/Project/Project-1
+
 call deploy.bat
 
