7a22e6932ddd32c06d21aafdbebe26d42e7d995c
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 14:24:42 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -3,12 +3,5 @@
 REM Write to C:\xyz.txt
 echo chal oye > C:\xyz.txt
 
-REM Check if deploy.bat exists in Project-1 directory and run it
-if exist C:\Project\Project-1\deploy.bat (
-    call C:\Project\Project-1\deploy.bat
-) else (
-    echo deploy.bat does not exist in C:\Project\Project-1
-)
-
-REM Similarly, add a check and call for deploy.bat in Project-2 if needed
-
+cd C:\Project\Project-1
+call deploy.bat
