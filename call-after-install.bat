42bd5b8af1eb5f25021b7f6492bdd5b0bb1b7720
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 11:30:44 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -1,3 +1,28 @@
 @echo off
-echo chal oye > C:\xyz.txt
+echo chal oye-2 > C:\xyz.txt
 call C:\after-install.bat
+
+REM Change the directory to the "Project" folder
+cd C:\Project
+
+REM Update and install dependencies for Project-1
+cd Project-1
+echo Updating Project-1...
+git pull
+npm install
+cd ..
+
+REM Update and install dependencies for Project-2
+cd Project-2
+echo Updating Project-2...
+git pull
+npm install
+cd ..
+
+REM Update and start the main Project
+echo Updating main Project...
+git pull
+npm start
+
+REM Pause to keep the command prompt window open
+pause
