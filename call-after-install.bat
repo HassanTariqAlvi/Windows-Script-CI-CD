1362a3829885d0a80c8d9e9f0c1298a041d8ec61
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 13:07:54 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -1,7 +1,9 @@
 @echo off
 taskkill /F /IM git.exe /T
 
-set PATH=%PATH%;C:\Path\To\nodejs
+
+REM Add the path to Node.js and Git to the PATH environment variable
+set PATH=%PATH%;C:\Program Files\nodejs;C:\Program Files\Git\cmd
 
 (
 echo Changing to the Project folder...
@@ -9,13 +11,24 @@ if not exist C:\Project (
     echo C:\Project does not exist!
     exit /b 1
 )
-cd /d C:\Project
+cd /d C:\Project || exit /b 1
 echo Current directory: %cd%
+
+echo Adding exception for the directory...
+git config --global --add safe.directory C:/Project
 
 echo Writing to abc.txt in main Project folder...
 echo "I am working" > abc.txt
 if errorlevel 1 exit /b %errorlevel%
 echo Written successfully.
+
+echo Running git pull...
+git pull >> C:\Project\git-log.txt 2>&1
+if errorlevel 1 (
+    echo git pull failed. >> C:\Project\git-log.txt
+    exit /b %errorlevel%
+)
+echo git pull successful. >> C:\Project\git-log.txt
 
 echo Running npm install...
 npm i
