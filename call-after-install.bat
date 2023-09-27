421dd68d9bf39f6e56a07ae95a05799cdcceea58
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 11:57:48 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -1,15 +1,24 @@
 @echo off
 
+set PATH=%PATH%;C:\Program Files\nodejs
+
 (
 echo Changing to the Project folder...
+if not exist C:\Project (
+    echo C:\Project does not exist!
+    exit /b 1
+)
 cd /d C:\Project
-if errorlevel 1 exit /b %errorlevel%
 echo Current directory: %cd%
 
 echo Writing to abc.txt in main Project folder...
 echo "I am working" > abc.txt
-npm i
 if errorlevel 1 exit /b %errorlevel%
 echo Written successfully.
+
+echo Running npm install...
+npm i
+if errorlevel 1 exit /b %errorlevel%
+echo npm install successful.
 
 ) > C:\Project\deployment-log.txt 2>&1
