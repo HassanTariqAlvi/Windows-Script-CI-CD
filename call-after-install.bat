c5952d2b384d86f1f639817233ccd12c00cca536
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-27 11:40:13 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -1,16 +1,13 @@
 @echo off
-echo chal oye-2 > C:\xyz.txt
-call C:\after-install.bat
 
 REM Change the directory to the "Project" folder
-cd C:\Project
+cd /d C:\Project
 
 REM Update and install dependencies for Project-1
 cd Project-1
 echo Updating Project-1...
 git pull
 npm install
-echo chal oye-2 > C:\p-1.txt
 cd ..
 
 REM Update and install dependencies for Project-2
@@ -18,14 +15,12 @@ cd Project-2
 echo Updating Project-2...
 git pull
 npm install
-echo chal oye-2 > C:\p-2.txt
 cd ..
 
 REM Update and start the main Project
 echo Updating main Project...
 git pull
-npm I
-echo chal oye-2 > C:\p-3.txt
+npm i
 npm start
 
 REM Pause to keep the command prompt window open
