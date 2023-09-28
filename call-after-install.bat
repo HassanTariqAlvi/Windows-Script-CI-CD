779f1318650c7434d4d9e9b15d2c277198a3ba1d
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-28 10:45:46 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -3,8 +3,9 @@
 REM Write to C:\xyz.txt
 echo Working > C:\xyz.txt
 
+
+
 REM Navigate to the project directory and execute git and npm commands
 cd C:\Project\Project-1
 
-REM Call deploy.bat and write logs to C:\Project\logs-git.txt
-call deploy.bat >> C:\Project\logs-git.txt 2>&1
+call deploy.bat
