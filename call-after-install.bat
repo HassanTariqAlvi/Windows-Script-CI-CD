231ef4cde42fff005602680c6bf17017ad576f6f
Muhammad Hassan Tariq <97219404+HassanTariqAlvi@users.noreply.github.com>
2023-09-28 16:20:22 +0500

    Update call-after-install.bat

--- call-after-install.bat
+++ call-after-install.bat
@@ -9,3 +9,9 @@ REM Navigate to the project directory and execute git and npm commands
 cd C:\Project\Project-1
 call deploy.bat
 
+cd C:\Project\Project-2
+call deploy.bat
+
+cd C:\Project
+call deploy.bat
+
